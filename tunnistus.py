fba1b8a888d975c2d2ea94f6e7db183bb91bbef9
ToniSarja <82703562+ToniSarja@users.noreply.github.com>
2022-09-09 16:08:19 +0300

    Update tunnistus.py

--- tunnistus.py
+++ tunnistus.py
@@ -2,13 +2,22 @@ import speech_recognition as sr
 import pyaudio
 
 import speech_recognition as sr
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    try:
-        audio = r.listen(source)
-        text = r.recognize_google(audio, language="ru")
-        print(text)
-        if text == 'Привет':
-            print('vittu')
-    except:
-        pass
+
+class Recog:
+    def __init__(self):
+        self.tunnista()
+
+    def tunnista(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            try:
+                audio = r.listen(source)
+                text = r.recognize_google(audio, language="ru")
+                print(text)
+                if text == 'Привет':
+                    print('Как дела')
+            except:
+                pass
+
+if __name__=='__main__':
+    Recog()
