73208c8cfe5eec44b8cc746044f026490733ba5f
ToniSarja <82703562+ToniSarja@users.noreply.github.com>
2022-11-02 11:36:28 +0200

    Update tunnistus.py

--- tunnistus.py
+++ tunnistus.py
@@ -59,19 +59,34 @@ class Recog:
             if current_time - self.speak_time >= self.speak_cooldown:
                 self.speak = False
 class Menu:
-	def valikko(self):
-		surface = pygame.display.set_mode((1000, 800))
-		menu = pygame_menu.Menu('', 800, 600,
-							theme=pygame_menu.themes.THEME_BLUE)
 
-		table = menu.add.table(table_id='my_table', font_size=20)
-		table.default_cell_padding = 5
-		table.default_row_background_color = 'white'
-		table.add_row(['First item', 'Second item', 'Third item'],
-					cell_font=pygame_menu.font.FONT_OPEN_SANS_BOLD)
-		table.add_row(['A', 'B', 1])
-		table.add_row(['α', 'β', 'γ'], cell_align=pygame_menu.locals.ALIGN_CENTER)
-		menu.mainloop(surface)
+    def paluu(self, game):
+        game = Game()
+        game.run()
+
+    def peruspeli(self):
+        self.display_surface.fill('black')
+        dt = self.clock.tick() / 1000
+        data = self.recordAudio()
+        text_surface = self.render(data)
+        self.display_surface.blit(text_surface, (0,50))
+        if data in otklik:
+            resp_surface = self.render(otklik[data]['otvet'])
+            self.display_surface.blit(resp_surface, (0,100))
+        self.all_sprites.update(dt)
+        self.all_sprites.draw(self.display_surface)
+
+    def valikko(self):
+        surface = pygame.display.set_mode((1000, 800))
+        menu = pygame_menu.Menu('', 800, 600,theme=pygame_menu.themes.THEME_BLUE)
+        menu.add.button('Exec', game.paluu, 'foo', align=pygame_menu.locals.ALIGN_LEFT)
+        table = menu.add.table(table_id='my_table', font_size=20)
+        table.default_cell_padding = 5
+        table.default_row_background_color = 'white'
+        table.add_row(['First item', 'Second item', 'Third item'], cell_font=pygame_menu.font.FONT_OPEN_SANS_BOLD)
+        table.add_row(['A', 'B', 1])
+        table.add_row(['α', 'β', 'γ'], cell_align=pygame_menu.locals.ALIGN_CENTER)
+        menu.mainloop(surface)
 
 
 class Game(Recog,Menu):
@@ -86,17 +101,7 @@ class Game(Recog,Menu):
     def render(self,x):
         return font.render(x, 1, (255,255,255))
 
-    def peruspeli(self):
-        self.display_surface.fill('black')
-        dt = self.clock.tick() / 1000
-        data = self.recordAudio()
-        text_surface = self.render(data)
-        self.display_surface.blit(text_surface, (0,50))
-        if data in otklik:
-            resp_surface = self.render(otklik[data]['otvet'])
-            self.display_surface.blit(resp_surface, (0,100))
-        self.all_sprites.update(dt)
-        self.all_sprites.draw(self.display_surface)
+
 
     def run(self):
         while True:
@@ -109,14 +114,15 @@ class Game(Recog,Menu):
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.peruspeli()
+                if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_m:
                         with open('sanasto.csv','a', encoding='utf8') as file:
                             sanasto = [str(self.sanat)]
                             writer = csv.writer(file)
                             writer.writerow(sanasto)
                             self.valikko()
-                else:
-                    self.peruspeli()
 
 
 
@@ -125,5 +131,6 @@ class Game(Recog,Menu):
 if __name__=='__main__':
     game = Game()
     game.run()
+
 
 
